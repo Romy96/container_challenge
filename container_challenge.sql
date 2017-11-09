70d450e0981271cc8b30fb68b771b7be69812624
ROMY\Romy96 <romybijkerk1996@gmail.com>
2017-11-09 16:07:02 +0100

    New version database

--- container_challenge.sql
+++ container_challenge.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Gegenereerd op: 07 nov 2017 om 14:26
+-- Gegenereerd op: 09 nov 2017 om 16:05
 -- Serverversie: 5.6.17
 -- PHP-versie: 5.5.12
 
@@ -34,6 +34,7 @@ CREATE TABLE IF NOT EXISTS `employees` (
   `email` varchar(250) DEFAULT NULL,
   `password` varchar(150) DEFAULT NULL,
   `phone_number` varchar(10) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
@@ -41,32 +42,35 @@ CREATE TABLE IF NOT EXISTS `employees` (
 -- Gegevens worden geëxporteerd voor tabel `employees`
 --
 
-INSERT INTO `employees` (`id`, `firstname`, `prefix`, `lastname`, `email`, `password`, `phone_number`) VALUES
-(1, 'Romy', NULL, 'Bijkerk', 'romy-bijkerk@hotmail.com', '27fa9d3a680e68b32cfe2cd22bbdba28', '0636010210'),
-(2, 'Tim', NULL, 'Bijkerk', 'tim_bijkerk@live.com', 'ddfee71cd43b45a1119c7ee8bbaceafe', '0636010210'),
-(3, 'Sandra', NULL, 'de Jager', 'sandradejager1969@gmail.com', '5b550718595a43218812fd0126930912', '06123456'),
-(4, 'Andre', NULL, 'van Donselaar', 'andre@live.com', '6bf5cb55b162dd19c6e428b347bfffee', '06123456');
+INSERT INTO `employees` (`id`, `firstname`, `prefix`, `lastname`, `email`, `password`, `phone_number`, `role_id`) VALUES
+(1, 'Romy', NULL, 'Bijkerk', 'romy-bijkerk@hotmail.com', '27fa9d3a680e68b32cfe2cd22bbdba28', '0636010210', 1),
+(2, 'Tim', NULL, 'Bijkerk', 'tim_bijkerk@live.com', 'ddfee71cd43b45a1119c7ee8bbaceafe', '0636010210', 2),
+(3, 'Sandra', NULL, 'de Jager', 'sandradejager1969@gmail.com', '5b550718595a43218812fd0126930912', '06123456', 3),
+(4, 'Andre', NULL, 'van Donselaar', 'andre@live.com', '6bf5cb55b162dd19c6e428b347bfffee', '06123456', 4);
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `employee_role`
+-- Tabelstructuur voor tabel `ports`
 --
 
-CREATE TABLE IF NOT EXISTS `employee_role` (
-  `employee_id` int(11) DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS `ports` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) DEFAULT NULL,
+  `adress` varchar(250) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `phone_number` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Gegevens worden geëxporteerd voor tabel `employee_role`
+-- Gegevens worden geëxporteerd voor tabel `ports`
 --
 
-INSERT INTO `employee_role` (`employee_id`, `role_id`) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4);
+INSERT INTO `ports` (`id`, `name`, `adress`, `country`, `phone_number`) VALUES
+(1, 'Haven Rotterdam', 'Wilhelminakade 909', 'Nederland', '06102521010'),
+(2, 'Haven Hong Kong', 'Tong Bin Ln 29', 'Hong Kong', '06202149358'),
+(3, 'Haven Singapore', 'Jurong port 32', 'Singapore', '0657289134');
 
 -- --------------------------------------------------------
 
@@ -93,6 +97,31 @@ INSERT INTO `roles` (`id`, `role`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `routes`
+--
+
+CREATE TABLE IF NOT EXISTS `routes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name_ship` varchar(250) DEFAULT NULL,
+  `start_port` varchar(50) DEFAULT NULL,
+  `destination_port` varchar(50) DEFAULT NULL,
+  `cargo` text NOT NULL,
+  `date_time_departure` timestamp(6) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name_ship` (`name_ship`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `routes`
+--
+
+INSERT INTO `routes` (`id`, `name_ship`, `start_port`, `destination_port`, `cargo`, `date_time_departure`) VALUES
+(1, 'Marc Mitchell', 'Haven Rotterdam', 'Haven Hong Kong', '', '0000-00-00 00:00:00.000000'),
+(6, 'Marc Mitchell', 'Haven Rotterdam', 'Haven Rotterdam', 'Kruiden, zout en medicijnen', '2017-11-11 12:00:00.000000');
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `ships`
 --
 
@@ -114,15 +143,16 @@ CREATE TABLE IF NOT EXISTS `ships` (
   `max_hazardous_substances_x` int(11) NOT NULL,
   `hold_max_total_floor_load_ton` int(11) NOT NULL,
   `hold_max_column_floor_load_ton` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `ships`
 --
 
 INSERT INTO `ships` (`id`, `IMO_number`, `name`, `year_build`, `home_port`, `home_country`, `MMSI_number`, `length_m`, `width_m`, `depth`, `load_capacity_ton`, `length_hold_m`, `width_hold_m`, `height_hold_m`, `max_hazardous_substances_x`, `hold_max_total_floor_load_ton`, `hold_max_column_floor_load_ton`) VALUES
-(1, '	9776171', 'OOCL HONG KONG', 2017, 'Hong Kong', 'HK', '477333500', 400, 59, 33, 191977, 300, 50, 23, 0, 0, 0);
+(2, '9187162', 'Marc Mitchell', 1998, 'Delfzijl', 'NL', '123456789', 93, 16, 6, 301, 80, 10, 4, 0, 0, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
